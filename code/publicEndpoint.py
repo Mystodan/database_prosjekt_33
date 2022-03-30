fa94de9bfa09b552a2b03bda326885d285f65a46
Daniel Hao Huynh <huynhdaniel10@gmail.com>
2022-03-30 20:02:10 +0200

    public endpoint

---
+++ code/publicEndpoint.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST']="localhost"
+app.config['MYSQL_USER']="root"
+app.config['MYSQL_PASSWORD']=""
+app.config['MYSQL_DB']="ski_equipment_manufacturer"
+
+mysql = MySQL(app)
+
+
+@app.route('/get_model',methods=['GET'])
+def get_model():
+    
+    if request.method == 'GET':
+
+        data = request.get_json()
+        order = "SELECT * FROM `skitype`"
+      
+        cur=mysql.connection.cursor()
+        
+        if len(data) > 0 : 
+          model = data['model']
+          order = order + "WHERE `model` = %s"
+          orders = cur.execute(order, [(model,)])
+        else :
+          orders = cur.execute(order,)
+
+        if orders > 0:
+            orders = cur.fetchall()
+
+        cur.close()
+        return jsonify(orders),201
+
+if __name__ == '__main__':
+    app.run(debug=True)
